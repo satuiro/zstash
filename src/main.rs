c917d5530736cea35e4494abe0860fb2ae3bafaa
satuiro <mohakmalhotra008@gmail.com>
2024-08-24 14:13:41 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let secure_dir = zstash::get_secure_dir();
     let _ = create_dir_all(&secure_dir).with_context(|| "Could not create a secure directory");
 
+    // println!("this is the main function");
     // create the command line interface using clap
     let matches = Command::new("zstash")
         .version("1.0")
@@ -16,24 +17,18 @@ fn main() {
         .about("Rust based secure file storage and sharing system")
         .subcommand(
             Command::new("encrypt")
-            .about("Encrypt a file")
-            .arg(Arg::new("file")
-            .required(true)
-            .index(1))
+                .about("Encrypt a file")
+                .arg(Arg::new("file").required(true).index(1)),
         )
         .subcommand(
             Command::new("decrypt")
                 .about("Decrypts a file")
-                .arg(Arg::new("file")
-                    .required(true)
-                    .index(1)),
+                .arg(Arg::new("file").required(true).index(1)),
         )
         .subcommand(
             Command::new("delete")
                 .about("Delete a file")
-                .arg(Arg::new("file")
-                    .required(true)
-                    .index(1)),
+                .arg(Arg::new("file").required(true).index(1)),
         )
         .get_matches();
 
@@ -42,16 +37,15 @@ fn main() {
         Some(("encrypt", sub_m)) => {
             let file = sub_m.get_one::<String>("file").expect("required argument");
             let _ = zstash::encrypt_file(file);
-        },
+        }
         Some(("decrypt", sub_m)) => {
             let file = sub_m.get_one::<String>("file").expect("required argument");
             let _ = zstash::decrypt_file(file);
-        },
+        }
         Some(("delete", sub_m)) => {
             let file = sub_m.get_one::<String>("file").expect("required argument");
             zstash::delete_file(file);
-        },
+        }
         _ => eprintln!("Invalid command. Use --help for more information."),
     }
-
 }
